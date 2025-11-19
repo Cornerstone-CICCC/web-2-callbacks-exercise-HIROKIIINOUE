db254dab522ab36984b6e2ef66b075974896c62e
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-18 20:25:50 -0800

    done exercise

--- file_reader.js
+++ file_reader.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
 
 fs.readFile("./firstname.txt", "utf-8", (err, data1) => {
-  // your code here
-})
+  const firstname = data1;
+  fs.readFile("./lastname.txt", "utf-8", (err, data2) => {
+    const lastname = data2;
+    fs.readFile("./age.txt", "utf-8", (err, data3) => {
+      const age = data3;
+      fs.readFile("./hobbies.txt", "utf-8", (err, data4) => {
+        const hobbies = JSON.parse(data4);
+
+        console.log(data4);
+        console.log(
+          `${firstname} ${lastname} is ${age} years old and his hobbies are ${hobbies[0]} and ${hobbies[1]}`
+        );
+      });
+    });
+  });
+});
